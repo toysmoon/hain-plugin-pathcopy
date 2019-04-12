222a251ee721f0acda7e57c021d84fc5d1fc2a55
sungjung <sungjung@peoplefund.co.kr>
2019-04-12 17:17:45 +0900

    fix: fileIndexer 비동기로 수정
    기존에 동기로 가져오던 file 정보를 비동기 promise 로 수정

--- fileIndexer.js
+++ fileIndexer.js
@@ -1,25 +1,24 @@
-const fs = require("fs");
+const fs = require('./fsWithPromise');
 
-async function getFileList(directoryPath) {
+async function getFileList(directoryPath, logger) {
     // blacklist
     const blackList = {
-        ".git": true
+        '.git': true
     };
 
-    // get fileList in current directory
-    const fileList = fs.readdirSync(directoryPath);
+    const fileList = await fs.readdir(directoryPath, logger);
     let enableFileList = [];
     let disableFileList = [];
 
     const isDirectoryList = fileList.map(async file => {
-        const fileStat = await fs.statSync(directoryPath + "/" + file);
+        const fileStat = await fs.stat(directoryPath + '/' + file);
         return fileStat.isDirectory();
     });
 
     for (let i = 0; i < isDirectoryList.length; i++) {
         const isDirectory = await isDirectoryList[i];
         const file = fileList[i];
-        const path = directoryPath + "/" + file;
+        const path = directoryPath + '/' + file;
         if (blackList[file]) {
             continue;
         }
@@ -30,13 +29,14 @@ async function getFileList(directoryPath) {
                   id: path,
                   primaryText: file,
                   secondaryText: path,
-                  group: "File Path Copy"
+                  group: 'File Path Copy',
+                  icon: ''
               });
     }
 
     // find all the Directory in parallel
     const directoryPromises = disableFileList.map(async path => {
-        return await getFileList(directoryPath + "/" + path);
+        return await getFileList(directoryPath + '/' + path);
     });
 
     // log them in sequence
